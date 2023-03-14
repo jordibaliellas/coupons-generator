882d129d38eab6d71ff1da81158efd358932f8f9
Jordi Baliellas <jordi.bp@endeavour.nl>
2023-03-14 23:19:09 +0100

    Improve sequential algorithm

--- src/coupons-generator/algorithms/sequential.ts
+++ src/coupons-generator/algorithms/sequential.ts
@@ -2,8 +2,9 @@ import { CouponGenerator } from '../interfaces/coupon.interface';
 
 export class SequentialCouponGenerator implements CouponGenerator {
   public *getCoupons(total: number): Generator<string> {
+    const totalLength = (total - 1).toString().length;
     for (let index = 0; index < total; index++) {
-      yield `${index}`;
+      yield `${index}`.padStart(totalLength, '0');
     }
   }
 }
