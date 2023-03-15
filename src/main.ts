d7bacc6a809cf8d7a1b802b0212a2777ec483e80
Jordi Baliellas <jordi.bp@endeavour.nl>
2023-03-15 09:47:52 +0100

    parse quantity and use it for the coupons

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CouponGeneratorController } from './coupons-generator/coupon-generator.controller';
 
+function convertQuantityToNumber(quantity: string): number {
+  const parsed = parseInt(quantity, 10);
+  if (isNaN(parsed)) {
+    throw new Error('Invalid quantity');
+  }
+  return parsed;
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const quantity = convertQuantityToNumber(configService.get('QUANTITY'));
+
   const couponController = app.get(CouponGeneratorController);
-  console.log('HIIII');
-  const res = couponController.getCoupons(11);
+  const res = couponController.getCoupons(quantity);
   console.log('REEES: ', res);
 }
 bootstrap();
